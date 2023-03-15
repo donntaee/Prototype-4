15eb496cdd8515c6124ccbe90718441deccf4882
donntaee <domasina@s.sfusd.edu>
2023-03-15 11:01:44 -0700

    qq

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
    public float speed = 5.0f;
+   public bool hasPowerup = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,4 +23,25 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"));
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }          
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collidedwith: " + collision.gameObject.name + "with power up set to " + hasPowerup);
+        }
+
+    
+    
+    }
+
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        float.spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float.spawnPosX = Random.Range(-spawnRange, spawnRange);
-
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-
-        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -22,4 +19,14 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+
 }
